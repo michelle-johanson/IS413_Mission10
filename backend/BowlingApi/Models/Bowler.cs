f7968db6afd92cdcc8c00d8c105edb5704b76ebe
michelle-johanson <mjohans0@byu.edu>
2026-03-18 11:23:08 -0600

    create the model

---
+++ backend/BowlingApi/Models/Bowler.cs
@@ -0,0 +1,14 @@
+namespace BowlingApi.Models;
+
+public class Bowler
+{
+    public string BowlerFirstName { get; set; } = "";
+    public string BowlerMiddleInit { get; set; } = "";
+    public string BowlerLastName { get; set; } = "";
+    public string TeamName { get; set; } = "";
+    public string BowlerAddress { get; set; } = "";
+    public string BowlerCity { get; set; } = "";
+    public string BowlerState { get; set; } = "";
+    public string BowlerZip { get; set; } = "";
+    public string BowlerPhoneNumber { get; set; } = "";
+}
